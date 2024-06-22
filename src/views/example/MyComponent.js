f9c18b317996d8305bf1c9dcea07c1ac5454a635
haihs-github <haihs.ptit@gmail.com>
2024-06-23 05:05:29 +0700

    video 3.4 react components

---
+++ src/views/example/MyComponent.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class MyComponents extends React.Component {
+
+	render() {
+		return (
+			<div>hello first component</div>
+		)
+	}
+
+}
+
+export default MyComponents;
