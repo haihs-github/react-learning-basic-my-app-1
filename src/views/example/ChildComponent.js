4a00dea71d65d63932d47cb035a65daffa10b73a
haihs-github <haihs.ptit@gmail.com>
2024-07-14 09:06:33 +0700

    video 19 css in react

--- src/views/example/ChildComponent.js
+++ src/views/example/ChildComponent.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import "./Demo.scss"
 class ChildComponent extends React.Component {
 	state = {
 		showJobs: false
@@ -52,7 +52,10 @@ class ChildComponent extends React.Component {
 			<>
 				{showJobs === false ?
 					<div>
-						<button onClick={() => this.handleShowHide()}>Show</button>
+						<button className="btn-show"
+							onClick={() => this.handleShowHide()}
+						>Show
+						</button>
 					</div>
 					:
 					<>
