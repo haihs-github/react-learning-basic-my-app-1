5e0772b20ddaf82c8b6eafb1b235f749757eb09e
haihs-github <haihs.ptit@gmail.com>
2024-08-11 14:46:15 +0700

    video #29 Importing Image

--- src/views/example/Home.js
+++ src/views/example/Home.js
@@ -1,10 +1,14 @@
 import React from "react";
+import logo from "../../asset/images/logo.png"
 
 class Home extends React.Component {
 	render() {
 		return (
 			<>
 				<div>Hello Home</div>
+				<div>
+					<img src={logo}></img>
+				</div>
 			</>
 		)
 	}
